581c3265d74de09d95dd20c4e1ec5ba610d22da2
bookinhat <46789631+bookinhat@users.noreply.github.com>
2019-01-18 23:11:52 +0530

    VB3

--- wp-content/plugins/premium-blocks-for-gutenberg/src/components/default-image.js
+++
@@ -1,9 +0,0 @@
-const placeHolder = PremiumBlocksSettings.defaultAuthImg;
-
-const { Component } = wp.element;
-
-export default class DefaultImage extends Component {
-  render() {
-    return <img src={placeHolder} />;
-  }
-}
